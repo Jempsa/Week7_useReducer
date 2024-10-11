a013c3d43db59d8e37ade495ddd8f28f1bee0630
Jempsa <t2koje02@students.oamk.fi>
2024-10-11 11:01:43 +0300

    Valmis

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer, useState } from 'react';
+import { View, TextInput, Button, FlatList, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const initialState = [];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, { id: Date.now(), text: action.payload }];
+    case 'REMOVE_TODO':
+      return state.filter(todo => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      dispatch({ type: 'ADD_TODO', payload: input });
+      setInput('');
+    }
+  };
+
+
+  const removeTodo = id => {
+    dispatch({ type: 'REMOVE_TODO', payload: id });
+  };
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.title}>TODO LIST</Text>
+
+
+      <TextInput
+        style={styles.input}
+        placeholder="Add new task..."
+        value={input}
+        onChangeText={setInput}
+      />
+
+    <TouchableOpacity style={styles.button} onPress={addTodo}>
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
+      
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => removeTodo(item.id)}>
+            <Text style={styles.todo}>{item.text}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 24, 
+    fontWeight: 'bold', 
+    textAlign: 'center', 
+    marginBottom: 20, 
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 30,
+    marginTop: 80,
+    backgroundColor: '#fff'
+  },
+  input: {
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  todo: {
+    padding: 10,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  button: {
+    backgroundColor: '#c1f7f0',
+    paddingVertical: 10,
+    borderRadius: 5,
+    width: '20%',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
